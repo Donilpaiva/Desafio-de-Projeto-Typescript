c16ce728908dd66ce0360decaa729fec0eef4538
Donilpaiva <donilpaiva@gmail.com>
2022-07-18 17:33:57 -0300

    Desafio de Projeto da DIO-Santander

---
+++ Desafio1/Desafio1.ts
@@ -0,0 +1,16 @@
+const employee: {name: string, code: number} = {
+    name: 'João',
+    code: 10
+}
+
+//ou
+
+interface Employee {
+    name: string,
+    code: number
+}
+
+const joao: Employee = {
+    name: 'João',
+    code: 10
+}
